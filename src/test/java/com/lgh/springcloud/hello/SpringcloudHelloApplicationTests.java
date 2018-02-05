4d3d4d57442a842eff855cdcde32dcd2ea9273af
Johnson Lau <gatement@gmail.com>
2018-02-05 22:45:04 +0800

    go

--- src/test/java/com/lgh/springcloud/hello/SpringcloudHelloApplicationTests.java
+++ src/test/java/com/lgh/springcloud/hello/SpringcloudHelloApplicationTests.java
@@ -1,16 +1,35 @@
 package com.lgh.springcloud.hello;
 
+
+import static org.hamcrest.Matchers.startsWith;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringcloudHelloApplicationTests {
 
+	private MockMvc mvc;
+
+	@Before
+	public void setUp() throws Exception {
+		mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+	}
+
 	@Test
-	public void contextLoads() {
+	public void hello() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(content().string(startsWith("hello world, ")));
 	}
 
 }
