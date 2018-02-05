eea421384ac4380710b05286c83d1344b937213e
liugh22 <guanhua.liu@midea.com>
2018-02-05 08:50:08 +0800

    go

---
+++ src/main/java/com/lgh/springcloud/hello/SpringcloudHelloApplication.java
@@ -0,0 +1,13 @@
+package com.lgh.springcloud.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudHelloApplication.class, args);
+	}
+	
+}
