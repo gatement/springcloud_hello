539033229a003124feffa42bac18b25838281c17
Johnson Lau <gatement@gmail.com>
2018-02-05 23:31:18 +0800

    go

--- src/main/java/com/lgh/springcloud/hello/HelloController.java
+++ src/main/java/com/lgh/springcloud/hello/HelloController.java
@@ -1,8 +1,10 @@
 package com.lgh.springcloud.hello;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableDiscoveryClient
 @RestController
 public class HelloController {
 
