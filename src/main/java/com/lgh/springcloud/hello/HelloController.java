3c0fc144d5f56b23af270df4cd7f365f314781c7
Johnson Lau <gatement@gmail.com>
2018-02-21 08:50:38 +0800

    go

--- src/main/java/com/lgh/springcloud/hello/HelloController.java
+++ src/main/java/com/lgh/springcloud/hello/HelloController.java
@@ -1,22 +1,27 @@
 package com.lgh.springcloud.hello;
 
+import java.util.Random;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.serviceregistry.Registration;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
 
-@EnableDiscoveryClient
 @RestController
 @Slf4j
 public class HelloController {
 	@Autowired
 	private Registration reg;
 
-	@RequestMapping("/hello")
-	public String index() {
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String hello() throws Exception {
+		int sleepTime = new Random().nextInt(3000);
+		log.info("sleepTime: {}", sleepTime);
+		Thread.sleep(sleepTime);
+
 		if (reg != null) {
 			log.info("Registration, host={}, port={}, uri={}, isSecure={}, serviceId={}, meta={}", reg.getHost(),
 					reg.getPort(), reg.getUri(), reg.isSecure(), reg.getServiceId(), reg.getMetadata());
